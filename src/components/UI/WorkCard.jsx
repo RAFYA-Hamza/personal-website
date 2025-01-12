86480c25c021a4053c054a3133f7402d5c607fc9
RYham <rafyahamza99@gmail.com>
2025-01-12 23:17:16 +0100

    Creation of a new UI component to isolate the work card

---
+++ src/components/UI/WorkCard.jsx
@@ -0,0 +1,45 @@
+import Tag from "./Tag";
+import IconButton from "./IconButton";
+import GithubIcon from "../icons/GithubIcon";
+import { SKILLS_ICONS } from "../../assets/icons/skills-icons";
+
+function WorkCard({ image, title, type, specificSkills, onSelect, classes }) {
+  return (
+    <div className={classes["work-card"]}>
+      <div className={classes["work-card__image"]}>
+        <img src={image} alt="Project 1" />
+      </div>
+
+      <div className={classes["work-card__content"]}>
+        <h3>{title}</h3>
+        <div className={classes["work-card__content-type"]}>
+          <p>Worked on</p>
+          <Tag>{type}</Tag>
+        </div>
+
+        <div className={classes["work-card__content-tech"]}>
+          <p>Skills & Technologies I have used</p>
+          <ul>
+            {specificSkills.map((index) => (
+              <li className="tag" key={index}>
+                {SKILLS_ICONS[index].icon}
+                <p className="body3 medium">{SKILLS_ICONS[index].title}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={classes["work-card__content-actions"]}>
+          <IconButton>
+            <GithubIcon />
+          </IconButton>
+          <button className="button" onClick={onSelect}>
+            Go to the {title}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkCard;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,12 @@
 import project1Img from "../assets/images/project1.png";
 
 import HeaderSection from "./UI/HeaderSection";
-import Tag from "./UI/Tag";
 import classes from "./Work.module.css";
 
-import { SKILLS_ICONS } from "../assets/icons/skills-icons";
+import WorkCard from "./UI/WorkCard";
 
 function Work() {
-  const specificSkills = [0, 1, 2, 13, 14, 15];
+  //   const specificSkills = [0, 1, 2, 13, 14, 15];
 
   function handleClick(url) {
     window.open(url, "_blank");
@@ -20,40 +19,16 @@ function Work() {
         description="Some of the projects I have worked on:"
       />
 
-      <div className={classes["work-card"]}>
-        <div className={classes["work-card__image"]}>
-          <img src={project1Img} alt="Project 1" />
-        </div>
-
-        <div className={classes["work-card__content"]}>
-          <h3>MarketiFy</h3>
-          <div className={classes["work-card__content-type"]}>
-            <p>Worked on</p>
-            <Tag>Website</Tag>
-          </div>
-
-          <div className={classes["work-card__content-tech"]}>
-            <p>Skills & Technologies I have used</p>
-            <ul>
-              {specificSkills.map((index) => (
-                <li className="tag" key={index}>
-                  {SKILLS_ICONS[index].icon}
-                  <p className="body3 medium">{SKILLS_ICONS[index].title}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <button
-            onClick={() =>
-              handleClick("https://rafya-hamza.github.io/marketify-website/")
-            }
-            className={`${classes["work-card__content-button"]} button`}
-          >
-            Go to the MarketiFY
-          </button>
-        </div>
-      </div>
+      <WorkCard
+        image={project1Img}
+        title="MarketiFy"
+        type="Website"
+        specificSkills={[0, 1, 2, 13, 14, 15]}
+        onSelect={() =>
+          handleClick("https://rafya-hamza.github.io/marketify-website/")
+        }
+        classes={classes}
+      />
     </section>
   );
 }
